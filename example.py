f84b47d2e23d9aff9d5435bd407bff29e3621b00
Fra <zuppif@usi.ch>
2018-12-10 23:34:00 +0100

    minor changes

--- example.py
+++ example.py
@@ -9,9 +9,13 @@ from torchvision.transforms import ToTensor, Resize, Compose
 model = vgg16(True)
 
 cat = Image.open("/home/francesco/Documents/mirror/mirror/resources/sky-dd.jpeg")
+#
+# cat = Image.open("/home/francesco/Documents/mirror/mirror/resources/the_starry_night-wallpaper-1920x1200.jpg")
 
 input = Compose([Resize((224,224)), ToTensor()])(cat)
+#
+# input = Compose([ToTensor()])(cat)
 
-input = input.view(1,3,224,224)
+input = input.unsqueeze(0)
 
 mirror(input, model)
