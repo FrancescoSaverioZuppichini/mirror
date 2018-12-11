047abb36d6b9e6821f7aebca338456514e194645
Fra <zuppif@usi.ch>
2018-12-11 14:03:40 +0100

    modified README, build web app. Ready to push

--- DummyVisualisation.py
+++ DummyVisualisation.py
@@ -3,18 +3,18 @@ from .Visualisation import Visualisation
 class DummyVisualisation(Visualisation):
 
     def __call__(self, inputs, layer):
-        return inputs.repeat(self.properties['params'][0]['value'],1, 1, 1)
+        return inputs.repeat(self.params['repeat']['value'],1, 1, 1)
 
     @property
     def name(self):
         return 'dummy'
 
     def init_params(self):
-        return [{ 'name' : 'repeat',
+        return {'repeat' : {
                  'type' : 'slider',
                  'min' : 1,
                  'max' : 100,
                  'value' : 3,
                  'step': 1,
-                 'params': []
-                 }]
+                 'params': {}
+                 }}
