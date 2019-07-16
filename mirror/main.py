1341f3c734be3ec2a30095367838aeee9fca29b7
francesco <zuppif@usi.ch>
2019-07-16 16:26:13 +0200

    added optional port argument

--- mirror/main.py
+++ mirror/main.py
@@ -3,9 +3,9 @@ import webbrowser
 from .App import App
 
 
-def mirror(input, model, visualisations=[]):
+def mirror(input, model, visualisations=[], port=5000):
     app = App(input, model, visualisations)
 
-    webbrowser.open_new('http://localhost:5000')  # opens in default browser
+    webbrowser.open_new(f'http://localhost:{port}')  # opens in default browser
 
-    app.run(host="0.0.0.0", port=5000, use_reloader=True)
+    app.run(host="0.0.0.0", port=5000)
