0bdadad75740a94599386bd6d80d0da0f93678e6
francesco <francesco.saverio.zuppichini@ch.pwc.com>
2020-02-23 14:25:36 +0100

    ModuleTraced
    - replace to ModuleHelper
    - no need to trace, we just need to create a DAG of the layers

--- mirror/App.py
+++ mirror/App.py
@@ -6,7 +6,7 @@ import logging
 from flask import Flask, request, Response, send_file, jsonify
 from torchvision.transforms import ToPILImage
 from .visualisations.web import Weights
-from .ModuleTracer import ModuleTracer
+from .ModuleHelper import ModuleHelper
 from .utils import device, add_batch
 
 
@@ -19,12 +19,16 @@ class App(Flask):
         if len(inputs) <= 0: raise ValueError('At least one input is required.')
         self.inputs, self.model = inputs, model
         self.device = device
-        self.module = model.to(self.device).eval()
         self.logger.setLevel(logging.INFO)
-        self.outputs: torch.Tensor = torch.empty(0)
+        with torch.no_grad():
+            self.module = model.to(self.device).eval()
+        self.outputs: torch.Tensor = torch.empty(0).to(self.device)
         self.input = add_batch(self.inputs[0]).to(self.device)  # add 1 dim for batch
-        self.layer = self.module
-        self.setup_tracer()
+        self.module = self.module
+        # instantiate a ModuleHelper object 
+        self.module_helper = ModuleHelper(module=self.module, device=self.device)
+        self.module_helper()
+        
         self.setup_visualisations(visualisations)
         self.cache = {vis.name: {} for vis in
                       self.visualisations}  # internal cache used to store the results of each visualization
@@ -35,7 +39,13 @@ class App(Flask):
 
         @self.route('/api/model', methods=['GET'])
         def api_model():
-            model = self.tracer.to_JSON()
+            """
+            Thid endpoint returns the JSON representation of the current model. 
+            
+            :return: A HTTP response with the JSON representation of the current model
+            :rtype: [type]
+            """
+            model = self.module_helper.serialize()
 
             response = jsonify(model)
 
@@ -43,14 +53,23 @@ class App(Flask):
 
         @self.route('/api/inputs', methods=['GET', 'PUT'])
         def api_inputs():
+            """
+            Thid endpoint returns the current visualization outputs if the method is GET or it change the current input if PUT.
+            
+            :return: A HTTP response carrying the current visualization outputs
+            :rtype: [type]
+            """
             response = Response('404', 'Invalid Method.')
 
             if request.method == 'GET':
+                # return the current visualization outputs
                 self.outputs = self.inputs
+                # by using the currenct timestamp in the reply link we ensure to not cache client side the images
                 response = [f'/api/model/image/{time.time()}/{i}' for i in range(len(self.outputs))]
                 response = jsonify({'links': response, 'next': False})
 
             elif request.method == 'PUT':
+                # change the current input based on the sent id
                 data = json.loads(request.data.decode())
                 input_index = data['id']
                 self.input = add_batch(self.inputs[input_index]).to(self.device)
@@ -61,14 +80,19 @@ class App(Flask):
         @self.route('/api/model/layer/<id>')
         def api_model_layer(id):
             id = int(id)
-            name = self.traced[id].name
+            name = self.module_helper[id].name
 
             return Response(response=name)
 
         @self.route('/api/visualisation', methods=['GET'])
         def api_visualisations():
+            """
+            This endpoint returns a list of visualisations. They are serialised (converted to JSON).
+            
+            :return: A HTTP response containing the serialised list of visualisations
+            :rtype: [type]
+            """
             serialised = [v.to_JSON() for v in self.visualisations]
-
             response = jsonify({'visualisations': serialised,
                                 'current': self.visualization.to_JSON()})
 
@@ -76,6 +100,12 @@ class App(Flask):
 
         @self.route('/api/visualisation', methods=['PUT'])
         def api_visualisation():
+            """
+            This endpoint set the params to the a visualisation.
+            
+            :return: A HTTP response containing the serialized updated visualization.
+            :rtype: [type]
+            """
             data = json.loads(request.data.decode())
             vis_name = data['name']
             try:
@@ -90,7 +120,7 @@ class App(Flask):
         @self.route('/api/model/layer/output/<id>')
         def api_model_layer_output(id):
             try:
-                self.layer = self.traced[id].module
+                self.module = self.module_helper[id].layer
                 self.outputs = self.get_outputs()
                 if len(self.outputs.shape) < 3: raise ValueError('Outputs must be a 3-D tensor.')
 
@@ -111,15 +141,34 @@ class App(Flask):
 
         @self.route('/api/model/image/<time>/<output_id>')
         def api_model_layer_output_image(time, output_id):
+            """This endpoint returns a single image from a specific visualization output 
+            defined by the parameter output_dir
+            
+            :param time: [description]
+            :type time: [type]
+            :param output_id: [description]
+            :type output_id: [type]
+            :return: [description]
+            :rtype: [type]
+            """
             output_id = int(output_id)
             try:
-                image = self.make_image(output_id)
+                output = self.outputs[output_id]
+                image = self.make_image(output)
                 return send_file(image, mimetype='image/jpeg')
             except KeyError:
                 return Response(status=500, response='Index not found.')
 
-    def make_image(self, output_id):
-        output = self.outputs[output_id].detach().cpu()
+    def make_image(self, image: torch.Tensor) -> io.BytesIO:
+        """
+        Create an image in memory from a tensor.
+        
+        :param output_id: [description]
+        :type output_id: [type]
+        :return: [description]
+        :rtype: [type]
+        """
+        output = image.detach().cpu()
         pil_img = ToPILImage()(output)
         img_io = io.BytesIO()
         pil_img.save(img_io, 'JPEG', quality=70)
@@ -129,22 +178,15 @@ class App(Flask):
     def get_outputs(self):
         vis_name = self.visualization.name
         vis_cache = self.cache[vis_name]
-        if (self.layer, self.input) not in vis_cache:
-            vis_cache[(self.layer, self.input)] = self.visualization(self.input.clone(),
-                                                                     self.layer)
+        if (self.module, self.input) not in vis_cache:
+            vis_cache[(self.module, self.input)] = self.visualization(self.input.clone(),
+                                                                     self.module)
             self.logger.info('Computing Visualisation')
         else:
             self.logger.debug('Cached')
 
-        outputs, _ = vis_cache[(self.layer, self.input)]
+        outputs, _ = vis_cache[(self.module, self.input)]
         return outputs
-
-    def setup_tracer(self):
-        # instantiate a Tracer object and trace one input
-        self.tracer = ModuleTracer(module=self.module, device=self.device)
-        self.tracer(self.input)
-        # store the traced graph as a dictionary
-        self.traced = self.tracer.__dict__()
 
     def setup_visualisations(self, visualisations):
         visualisations = [*self.default_visualisations, *visualisations]
---
+++ mirror/ModuleHelper.py
@@ -0,0 +1,78 @@
+import sys
+
+
+class LayerNode():
+    def __init__(self, parent=None):
+        self.parent = parent
+        self.children = []
+
+    @property
+    def id(self):
+        return str(id(self.layer))
+
+    @property
+    def name(self):
+        return str(self.layer)
+
+    def __dict__(self):
+        """
+        Recursively add items with not children to flat the dictionary.
+        :return:
+        """
+        v = {self.id: self}
+        for child in self.children:
+            v = {**v, **child.__dict__()}
+        return v
+
+    def to_JSON(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'children': [child.to_JSON() for child in self.children]
+        }
+
+
+class ModuleHelper():
+    """
+    This class is able to serialize a given module and to create a graph of LayerNode in order
+    to look for a specific layer given an id
+    """
+
+    def __init__(self, module, device):
+        super().__init__()
+        self.module = module
+        self.handles = []
+        self.root = LayerNode(parent=None)
+        self.device = device
+
+    def trace(self, module, root=None):
+        """
+        Recursively creates a tree of TracedLayer
+        :param module: Current module
+        :param root: Parent Node
+        :return: New Node
+        """
+        root.layer = module
+        for child in module.children():
+            root.children.append(self.trace(child, LayerNode(parent=root)))
+        return root
+
+    def __call__(self):
+        self.root = self.trace(self.module, self.root)
+        self.name2layers = self.root.__dict__()
+        return self
+
+    def __repr__(self):
+        return self.to_JSON().__repr__()
+    
+    def clean(self):
+        [h.remove() for h in self.handles]
+
+    def __getitem__(self, idx):
+        return self.name2layers[idx]
+
+    def serialize(self):
+        return self.to_JSON()
+
+    def to_JSON(self):
+        return self.root.to_JSON()
